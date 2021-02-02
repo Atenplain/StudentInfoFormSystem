9c0f796023ab15ec806e6368bbb71023837fa40a
fengye404 <1129126684@qq.com>
2021-02-02 18:14:22 +0800

    1-21

---
+++ src/main/java/org/sacc/smis/entity/Passwords.java
@@ -0,0 +1,15 @@
+package org.sacc.smis.entity;
+
+import lombok.Data;
+
+/**
+ * @author ：風楪fy
+ * @date ：Created in 2021/2/2 - 18:01
+ */
+@Data
+public class Passwords {
+
+    private String oldPassword;
+
+    private String newPassword;
+}
